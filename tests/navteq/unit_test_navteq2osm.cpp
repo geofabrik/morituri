204e36006989f1eb219267f6dd0b02b1e59bfc83
Philip Beelmann <philip@geofabrik.de>
2015-11-10 16:36:25 +0100

    more tests

--- tests/navteq/unit_test_navteq2osm.cpp
+++ tests/navteq/unit_test_navteq2osm.cpp
@@ -6,21 +6,36 @@
 
 
 TEST_CASE("Create nodes for administrative boundaries", "[admin_nodes]") {
+    std::vector<int> ring_sizes = { 2, 5, 10, 100, 999, 1000, 1001, 1002, 10000 };
+    for (auto ring_size : ring_sizes) {
+        OGRLinearRing* ring = new OGRLinearRing();
+        OGRPoint* p = new OGRPoint(0, 0);
+        ring->addPoint(p);
+        for (auto i = 1; i < ring_size; i++) {
+            ring->addPoint(new OGRPoint(i, i));
+        }
+        ring->addPoint(p);
 
-    int ring_size = 10005;
+        node_vector_type osm_ids = create_admin_boundary_way_nodes(ring);
 
-    OGRLinearRing* ring = new OGRLinearRing();
-    OGRPoint* p = new OGRPoint(0,0);
-    ring->addPoint(p);
-    for (auto i=1; i<ring_size; i++){
-        ring->addPoint(new OGRPoint(i,i));
+        CHECK(osm_ids.size() == ring->getNumPoints());
     }
-    ring->addPoint(p);
+}
 
-    node_vector_type osm_ids = create_admin_boundary_way_nodes(ring);
+TEST_CASE("Create way for administrative boundaries", "[admin_ways]") {
+    std::vector<int> ring_sizes = { 2, 5, 10, 100, 999, 1000, 1001, 1002, 10000 };
+    for (auto ring_size : ring_sizes) {
+        OGRLinearRing* ring = new OGRLinearRing();
+        OGRPoint* p = new OGRPoint(0, 0);
+        ring->addPoint(p);
+        for (auto i = 1; i < ring_size; i++) {
+            ring->addPoint(new OGRPoint(i, i));
+        }
+        ring->addPoint(p);
+        osm_id_vector_type osm_way_ids = create_admin_boundary_ways(ring);
 
-
-    CHECK(osm_ids.size() == ring->getNumPoints());
-
+        int number_of_way_ids = floor(ring->getNumPoints() / OSM_MAX_WAY_NODES)+1;
+        CHECK(osm_way_ids.size() == number_of_way_ids);
+    }
 }
 
